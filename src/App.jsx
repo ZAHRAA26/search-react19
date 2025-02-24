c69d08c52530ee2cbae8db20b4f87a3288c731ec
zahraa <zahraa.elsayed26@yahoo.com>
2025-02-24 13:22:11 +0300

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,90 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from "react";
 
-import './App.css'
+import "./App.css";
 import Search from "./components/Search.jsx";
-
+import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
+import { useDebounce } from "react-use";
 function App() {
-const [searchTerm,setSearchTerm]=useState('');
+  const BASE_URL = "https://api.themoviedb.org/3";
+  const API_KEY = import.meta.env.VITE_TMBD_API_KEY;
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [moviesList, setMoviesList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+  // Debounce the search term to prevent making too many API requests
+  // by waiting for the user to stop typing for 500ms
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+  const fetchMovies = async (query = "") => {
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+      const endPoint = query
+        ? `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}`
+        : `${BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endPoint, options);
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error("failed to" + " fetch" + " movies");
+      }
+      if (data.response === false) {
+        setErrorMessage(data.error || "failed to fetch data");
+        setMoviesList([]);
+        return;
+      }
+      setMoviesList(data.results);
+    } catch (e) {
+      console.log(`error fetching movies:${e}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
   return (
-
-        <main>
-            <div className='pattern'/>
-            <div className='wrapper'>
-                <header>
-                    <img src='../public/hero.png' alt='Hero Banner'/>
-                    <h1>
-                        Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle
-                    </h1>
-                </header>
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-                <h1 className='text-white'> {searchTerm}</h1>
-            </div>
-
-
-        </main>
-
-
-  )
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without The Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <h1 className="text-white"> {searchTerm}</h1>
+        <section className="all-movies">
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">${errorMessage}</p>
+          ) : (
+            <>
+              <h2 className="mt-[40px]">
+                {searchTerm ? `Search Result` : `All Movies`}
+              </h2>
+              <ul>
+                {moviesList.map((movie) => (
+                  <MovieCard movie={movie} key={movie.id} />
+                ))}
+              </ul>
+            </>
+          )}
+        </section>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Search = ({searchTerm,setSearchTerm}) => {
-    return (
-        <div className='search'>
-            <div>
-                <img src="../../public/search.svg" alt="search"/>
-                    <input type='text' placeholder='search through thouthands of movies' value={searchTerm} onChange={e=>{setSearchTerm(e.target.value)}}/>
-            </div>
-        </div>
-    )
-}
-export default Search
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="../../public/search.svg" alt="search" />
+        <input
+          type="text"
+          placeholder="search through thouthands of movies"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+export default Search;
